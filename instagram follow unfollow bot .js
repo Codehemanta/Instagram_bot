48f9d0f8471c94dd53179ee04d7e40bc99505086
Hemanta_Gayen <69314427+Codehemanta@users.noreply.github.com>
2020-08-12 19:28:43 +0530

    Update instagram follow unfollow bot .js

--- instagram follow unfollow bot .js
+++ instagram follow unfollow bot .js
@@ -1,24 +1,19 @@
 // 2 secend
 let timer = 2000;
-//follow button select
-let followbtn = document.querySelector('._5f5mN');
-//unfollow button select
-let unfollowbtn = document.getElementsByClassName('aOOlW ')[0];
-// i<20 is number of repit 
 for (var i = 0; i <20 ; i++) {
 	setTimeout(()=>{
-		followbtn.click()
+		document.getElementsByClassName('_5f5mN')[0].click()
 		console.log('follow')
 	},timer)
 	//add extra time
 	timer +=2000;
 	setTimeout(()=>{
-		followbtn.click()
+		document.getElementsByClassName('_5f5mN')[0].click()
 	},timer)
 	timer +=2000;
 
 	setTimeout(()=>{
-		unfollowbtn.click()
+		document.getElementsByClassName('aOOlW ')[0].click()
 		console.log('unfollow')
 	},timer)
 	timer +=2000;
